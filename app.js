15dae3b447563172d71e32d6807819f1a99b0cb1
SyedBashar <MhuammadBashar@gmail.com>
2024-10-08 16:45:12 +0500

    Done alert Assigmnet

---
+++ app.js
@@ -0,0 +1,9 @@
+alert("Error! Please enter a valid password.")
+alert("Welcome to JS Land... /n Happy Coding ")
+alert("Welcome to JS Land")
+alert("Hello I can run JS through my web browser's console")
+
+
+
+
+
